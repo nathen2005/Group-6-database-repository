f62671891cbd5729b77bec833fc188597f09689b
mohaz05 <abdi.hamza@strathmore.edu>
2024-10-19 09:10:52 +0300

    Add files via upload

--- Waste Management.sql
+++ Waste Management.sql
@@ -7,7 +7,6 @@ Dateofcollection INT,
 Typeofwastecollected VARCHAR(255)
 )
 
-
 Create Table Recycling (
 Typeofwastecollected VARCHAR(255),
 Dateofcollection INT,
@@ -15,16 +14,31 @@ AddressID INT,
 ClientID INT
 )
 
+
 Create Table Client(
 ClientID INT,
 ClientName VARCHAR(255),
 AddressID INT,
 City VARCHAR(255),
-ContactDetailsforClient DOUBLE
+ContactDetailsforClient INT
 )
 
 Create Table Staff(
 StaffID INT,
 StaffName VARCHAR(255),
-ContactdetailsforStaff DOUBLE
+ContactdetailsforStaff INT
 )
+
+Alter Table Client
+ADD ClientEmail VARCHAR(255);
+ADD Estate VARCHAR(255);
+
+Create Table Payments(
+PaymentID INT,
+ClientID INT,
+Typeofwastecollected VARCHAR(255),
+Pointsearned INT
+)
+
+ALTER TABLE Recycling
+ADD Pointsearned INT;
