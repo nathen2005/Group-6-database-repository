bcd2525104a5d43e1014005e522e3f3166dda2d2
IbnJaldun <167401324+IbnJaldun@users.noreply.github.com>
2024-11-16 21:03:16 +0300

    Update Waste collection data entries.sql

--- Waste collection data entries.sql
+++ Waste collection data entries.sql
@@ -1,23 +1,23 @@
 -- Insert data into the Client table
 INSERT INTO Client (ClientID, ClientName, AddressID, City, ContactDetailsforClient)
 VALUES
-  (1, 'Company A', 101, 'City A', 1234567890),
-  (2, 'Company B', 102, 'City B', 9876543210),
-  (3, 'Company C', 103, 'City C', 5555555555);
+  (10, 'Company A', 101, 'Madaraka', 0734567890),
+  (11, 'Company B', 102, 'South  B', 0776543210),
+  (12, 'Company C', 103, 'Nairobi We', 0755555555);
 
 -- Insert data into the Staff table
 INSERT INTO Staff (StaffID, StaffName, ContactdetailsforStaff)
 VALUES
-  (101, 'John Doe', 1234567891),
-  (102, 'Jane Smith', 9876543211),
-  (103, 'Mike Johnson', 5555555556);
+  (101, 'John Doe', 0734567891),
+  (102, 'Jane Smith', 0776543211),
+  (103, 'Mike Johnson', 0755555556);
 
 -- Insert data into the Waste_Management table
 INSERT INTO Waste_Management (CollectionID, ClientID, AddressID, StaffID, Dateofcollection, Typeofwastecollected)
 VALUES
-  (1, 1, 101, 101, 20231111, 'Paper'),
-  (2, 2, 102, 102, 20231112, 'Plastic'),
-  (3, 3, 103, 103, 20231113, 'Glass');
+  (1, 10, 101, 101, 20231111, 'Paper'),
+  (2, 11, 102, 102, 20231112, 'Plastic'),
+  (3, 12, 103, 103, 20231113, 'Glass');
 
 -- Insert data into the Recycling table
 INSERT INTO Recycling (Typeofwastecollected, Dateofcollection, AddressID, ClientID)
